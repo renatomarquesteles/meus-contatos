d859387653e7e3d2a4862118c6f86a494983fe93
Renato <renatomarquesteles@gmail.com>
2020-04-10 14:28:40 -0300

    backend: feat: cria autenticacao e login

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '8ef05b6743be20d14a619ad86fce70b2',
+  expiresIn: '7d',
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,13 +3,19 @@ import multer from 'multer';
 
 import ContactController from './app/controllers/ContactController';
 import FileController from './app/controllers/FileController';
+import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
