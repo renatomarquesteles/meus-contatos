2135b9d82b4c082afb41a1e42a6cb10da6225f1e
Renato <renatomarquesteles@gmail.com>
2020-04-11 13:26:36 -0300

    frontend: chore: add rotas privadas

---
+++ frontend/src/pages/NewContact/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NewContact() {
+  return <div>Novo contato</div>;
+}
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Contacts from '../pages/Contacts';
 import Login from '../pages/Login';
+import NewContact from '../pages/NewContact';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
+        <Route path="/contacts" exact component={Contacts} isPrivate />
+        <Route path="/contacts/new" component={NewContact} isPrivate />
       </Switch>
     </BrowserRouter>
   );
