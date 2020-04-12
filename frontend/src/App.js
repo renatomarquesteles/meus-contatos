f5b90d580c57e36332dbf97397067364d8bc8d8f
Renato <renatomarquesteles@gmail.com>
2020-04-12 19:34:30 -0300

    frontend: chore: add estilo global

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { Router } from 'react-router-dom';
 import history from './services/history';
 
 import Routes from './routes';
+import GlobalStyles from './styles/global';
 import './config/ReactotronConfig';
 
 function App() {
   return (
     <Router history={history}>
       <Routes />
+      <GlobalStyles />
     </Router>
   );
 }
