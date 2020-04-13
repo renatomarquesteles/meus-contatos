99db085a8402ecc00f360de109262087936cc9ad
Renato <renatomarquesteles@gmail.com>
2020-04-13 20:34:00 -0300

    frontend: chore: add mask nos inputs

--- frontend/src/components/FormInput/index.js
+++ frontend/src/components/FormInput/index.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useRef } from 'react';
+import InputMask from 'react-input-mask';
 import { useField } from '@unform/core';
 
 import { ErrorMessage, InputContainer, InputWrapper } from './styles';
 
-export default function Input({ name, label, icon: Icon, ...rest }) {
+export default function Input({
+  name,
+  label,
+  mask,
+  maskPlaceholder = null,
+  icon: Icon,
+  ...rest
+}) {
   const inputRef = useRef(null);
   const { fieldName, defaultValue = '', registerField, error } = useField(name);
 
@@ -20,13 +28,25 @@ export default function Input({ name, label, icon: Icon, ...rest }) {
       <label htmlFor={name}>{label}</label>
       <InputWrapper>
         {Icon && <Icon size={20} color="#444" />}
-        <input
-          id={name}
-          name={name}
-          ref={inputRef}
-          defaultValue={defaultValue}
-          {...rest}
-        />
+        {mask ? (
+          <InputMask
+            mask={mask}
+            maskPlaceholder={maskPlaceholder}
+            id={name}
+            name={name}
+            ref={inputRef}
+            defaultValue={defaultValue}
+            {...rest}
+          />
+        ) : (
+          <input
+            id={name}
+            name={name}
+            ref={inputRef}
+            defaultValue={defaultValue}
+            {...rest}
+          />
+        )}
       </InputWrapper>
       <ErrorMessage>{error}</ErrorMessage>
     </InputContainer>
