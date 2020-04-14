83fc9351a162b2b48e7e4a746ff29a66da9e9303
Renato <renatomarquesteles@gmail.com>
2020-04-14 14:47:39 -0300

    frontend: chore: adiciona redux e redux-saga

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
-import history from './services/history';
+import { Provider } from 'react-redux';
+
+import './config/ReactotronConfig';
 
 import Routes from './routes';
+import store from './store';
 import GlobalStyles from './styles/global';
-import './config/ReactotronConfig';
 
 function App() {
   return (
-    <Router history={history}>
+    <Provider store={store}>
       <Routes />
       <GlobalStyles />
-    </Router>
+    </Provider>
   );
 }
 
