ccde0678c3ca7315f70de9044eeb690b8cb1ae24
Renato <renatomarquesteles@gmail.com>
2020-04-14 15:42:54 -0300

    frontend: chore: persistindo dados da autenticacao

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 import history from './services/history';
-import store from './store';
+import { persistor, store } from './store';
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-      </Router>
-      <GlobalStyles />
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+        </Router>
+        <GlobalStyles />
+      </PersistGate>
     </Provider>
   );
 }
