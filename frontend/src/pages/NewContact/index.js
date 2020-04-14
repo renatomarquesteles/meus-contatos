3c9339dea96dfc9e6c2543edd16e2efe9e0e1693
Renato <renatomarquesteles@gmail.com>
2020-04-13 21:40:17 -0300

    frontend: feat: add pagina de novo contato estilizada

--- frontend/src/pages/NewContact/index.js
+++ frontend/src/pages/NewContact/index.js
@@ -1,5 +1,106 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  MdAccountCircle,
+  MdAddCircleOutline,
+  MdEmail,
+  MdPersonAdd,
+} from 'react-icons/md';
+import { Form } from '@unform/web';
+
+import Button from '../../components/Button';
+import PhoneInput from '../../components/PhoneInput';
+import Input from '../../components/FormInput';
+
+import {
+  AddButtonContainer,
+  AddressContainer,
+  AddressInfo,
+  CityStateWrapper,
+  Container,
+  Content,
+} from './styles';
 
 export default function NewContact() {
-  return <div>Novo contato</div>;
+  const formRef = useRef(null);
+
+  return (
+    <Container>
+      <Content>
+        <h1>Criar um novo contato</h1>
+        <p>
+          Insira os dados de um novo contato e o adicione à sua lista de
+          contatos!
+        </p>
+        <Form ref={formRef} onSubmit={() => {}}>
+          <Input
+            name="name"
+            label="Nome"
+            placeholder="John Doe"
+            icon={MdAccountCircle}
+          />
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            placeholder="exemplo@email.com"
+            icon={MdEmail}
+          />
+          <PhoneInput name="phone" label="Telefone/Celular" />
+          <AddButtonContainer>
+            <button type="button">
+              <MdAddCircleOutline size={24} color="#444" />
+            </button>
+          </AddButtonContainer>
+          <AddressContainer>
+            <label>Endereço</label>
+            <AddressInfo>
+              <Input
+                name="zipcode"
+                label="CEP"
+                mask="99999-999"
+                maskPlaceholder="_____-___"
+                placeholder="99999-999"
+              />
+              <Input
+                name="street"
+                label="Logradouro"
+                placeholder="Rua São José"
+              />
+              <Input
+                name="complement"
+                label="Complemento"
+                placeholder="Informações adicionais"
+              />
+              <Input name="neighborhood" label="Bairro" placeholder="Centro" />
+              <CityStateWrapper>
+                <Input name="city" label="Cidade" placeholder="São Paulo" />
+                <Input
+                  name="state"
+                  label="Estado"
+                  mask="aa"
+                  maskPlaceholder="_"
+                  placeholder="SP"
+                />
+              </CityStateWrapper>
+            </AddressInfo>
+            <AddButtonContainer>
+              <button type="button">
+                <MdAddCircleOutline size={24} color="#444" />
+              </button>
+            </AddButtonContainer>
+          </AddressContainer>
+          <Button type="submit">
+            <span>SALVAR CONTATO</span>
+            <MdPersonAdd size={22} color="#fff" />
+          </Button>
+          <Link to="/contacts">
+            <Button type="button" secondary>
+              <span>CANCELAR</span>
+            </Button>
+          </Link>
+        </Form>
+      </Content>
+    </Container>
+  );
 }
---
+++ frontend/src/pages/NewContact/styles.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+import colors from '../../styles/colors';
+
+export const Container = styled.div`
+  padding-bottom: 100px;
+  margin-top: 15px;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 560px;
+  background: ${colors.container};
+  margin: 0 auto;
+  padding: 20px 30px;
+  border-radius: 10px;
+
+  h1 {
+    font-size: 24px;
+    font-weight: bold;
+    color: ${colors.primaryText};
+  }
+
+  > p {
+    margin: 20px 0;
+    color: ${colors.secondaryText};
+  }
+
+  button {
+    width: 100%;
+    margin: 10px 0;
+  }
+`;
+
+export const AddButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  button {
+    background: none;
+    border: 0;
+    width: fit-content;
+
+    svg {
+      transition: fill 0.2s linear;
+    }
+
+    &:hover {
+      svg {
+        fill: ${colors.primary};
+      }
+    }
+  }
+`;
+
+export const AddressContainer = styled.div`
+  > label {
+    font-weight: bold;
+    color: ${colors.primaryText};
+  }
+`;
+
+export const AddressInfo = styled.div`
+  margin-top: 5px;
+  padding: 0 10px 10px;
+  border-radius: 4px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+`;
+
+export const CityStateWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+  grid-gap: 10px;
+
+  > div:last-of-type {
+    input {
+      text-transform: uppercase;
+    }
+  }
+`;
